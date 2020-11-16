ee94563f30612de2345c22cdf426ce918f7edd13
shen huang <shenyh33@gmail.com>
2020-11-16 10:19:58 -1000

    commit tree beb407920fbe4e93617ebc5e8dea1c3f1c0eeb09

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -5,9 +6,67 @@ class Song
   def self.all
     @@all
   end
-
+  
   def save
-    self.class.all << self
+    self.class.all << self 
+  end
+
+  def self.create
+    song = self.new
+    self.all << song
+    song
+  end
+
+  def self.new_by_name(title)
+    song = self.new
+    song.name = title
+    song
+  end
+
+  def self.create_by_name(song_name)
+    song = self.create
+    song.name = song_name
+    song
+  end
+
+  def self.find_by_name(title)
+    result = self.all.detect { |song| song.name == title}
+    result
+  end
+
+  def self.find_or_create_by_name(title)
+    result = self.find_by_name(title)
+    if result
+      result
+    else
+      self.create_by_name(title)
+    end
+  end
+
+  def self.alphabetical
+    sorted = self.all.sort_by { |song| song.name }
+    sorted
+  end
+
+  def self.new_from_filename(filename)
+    song_array = filename.split(" - ")
+    song_array[1] = song_array[1].chomp(".mp3")
+    song = self.new
+    song.name = song_array[1]
+    song.artist_name = song_array[0]
+    song
+  end
+
+  def self.create_from_filename(filename)
+    result = self.new_from_filename(filename)
+    song = self.create
+    song.name = result.name
+    song.artist_name = result.artist_name
+    song
+  end
+
+  def self.destroy_all
+    self.all.clear
   end
 
 end
